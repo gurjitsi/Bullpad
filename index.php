2c61b495fabfdb0c1f399833c79629f4a8e0fa80
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2022-07-24 15:38:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ function validate()
               </div> 
              <div style='text-align:left;float:left;width:200px;position:relative;z-index: 1;margin-top:20px;'>
                <span style='font-size:18px;font-weight:700;color:#1A6981;'>Bullpad</span><br/>
-               <span style='font-size:10px;font-weight:500;'>complete education technology solution
+               <span style='font-size:10px;font-weight:500;'>Complete education technology solution
               </div>
 		<div style='text-align:right;float:right;width:700px;'>
                  <a href="admin/index.php" style='display: inline-block;padding: 2px 10px;background: #1A6981;border-radius: 5px;color:white;text-decoration:none;padding:5px 5px 5px 5px;'>Principal Login</a> &nbsp; <a href="user/index.php" style='display: inline-block;padding: 2px 10px;background: #1A6981;border-radius: 5px;color:white;text-decoration:none;padding:5px 5px 5px 5px;'>Teacher Login</a> &nbsp; <a href="student/index.php" style='display: inline-block;padding: 2px 10px;background: #1A6981;border-radius: 5px;color:white;text-decoration:none;padding:5px 5px 5px 5px;'>Student / Parents Login</a>
@@ -54,8 +54,8 @@ function validate()
 <div id="banner"> 
 	<div id="banner-data"><br/>
 		<div style="text-align:center;width:310px;margin-left:auto;margin-right:auto;">
-                 <img src='images/Droitwich_Spa_High_School_logo.png' height='200px' width='200px'><br/><br/>
-		<span style="color:#13A7C7;font-size:22px;">Droitwich </span><span style="color:#444;font-size:22px;">Public High School</span><br/><br/>
+                 <img src='images/abschool.png' height='200px' width='200px'><br/><br/>
+		<span style="color:#13A7C7;font-size:22px;">Techno </span><span style="color:#444;font-size:22px;">Public High School</span><br/><br/>
    			 
 	</div>
 	</div>
@@ -68,7 +68,7 @@ function validate()
 <div style="width:100%;height:100px;background-color:#444;">
     <div style="width:1000px;margin-left:auto;margin-right:auto;margin-top:10px;padding:10px;font-size:12px;color:#ffffff;font-family: 'Trebuchet MS', Arial;">
        <div style='float:left;width:50%;text-align:left;'>
-       Copyright &copy; 2013, Bullpad Technologies Pvt. Ltd. All rights reserved.
+       Copyright &copy; 2013-2022, Gurjit Singh. All rights reserved.
       </div> 
        </div>
 </div>
